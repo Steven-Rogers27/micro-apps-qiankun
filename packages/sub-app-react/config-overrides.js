f18dff4f8b3231add42a6be80425caca42ae44c5
孙伟(10034471) <sunw-d@glodon.com>
2021-01-07 11:26:01 +0800

    修改tsconfig

--- packages/sub-app-react/config-overrides.js
+++ packages/sub-app-react/config-overrides.js
@@ -1,4 +1,8 @@
 const path = require('path');
+const fs = require('fs');
+const {
+  getBabelLoader,
+} = require('customize-cra');
 
 module.exports = {
   webpack: function(config, env) {
@@ -8,6 +12,51 @@ module.exports = {
       ...config.resolve.alias,
       '@': path.resolve(__dirname, 'src'),
     };
+    // 为了兼容IE11，据说是react和react-dom的所依赖的Symbol polyfill版本不一样，需要强制依次引入core-js（因为@babel/polyfill即将作废，其内部
+    // 还是引用的core-js和regenerator-runtime）、react、react-dom，以保证react和react-dom使用同样的Symbol polyfill
+    // issue: https://stackoverflow.com/questions/40897966/objects-are-not-valid-as-a-react-child-in-internet-explorer-11-for-react-15-4-1
+    config.entry = [
+      'react',
+      'react-dom',
+      config.entry,
+    ];
+    // 处理src目录下的js文件的babel-loader配置
+    const babelLoader = getBabelLoader(config);
+    babelLoader.options.presets.push(
+      [
+        '@babel/env',
+        {
+          useBuiltIns: 'usage',
+          corejs: '3',
+          targets: {
+            ie: 9,
+          },
+          modules: 'umd',
+        }
+      ],
+      [
+        '@babel/preset-react',
+        {
+          development: process.env.NODE_ENV === 'development',
+        }
+      ],
+      [
+        '@babel/preset-typescript',
+        {
+          isTSX: true,
+          allExtensions: true,
+        }
+      ],
+    );
+    // 处理src目录以外的js文件的babel-loader配置
+    const babelLoaderOuter = getBabelLoader(config, true);
+    babelLoaderOuter.options.plugins = babelLoaderOuter.options.plugins || [];
+    babelLoaderOuter.options.plugins.push(
+      [
+        '@babel/plugin-transform-runtime'
+      ],
+    );
+
     return config;
   },
 
